1a07116115b00244d7f9650ebf1433432dcb0f78
Joel Small <joel.small@biscon.com.au>
2017-02-10 19:22:42 +1030

    Bugfixes ahead of next release

--- Relate.php
+++ Relate.php
@@ -7,10 +7,11 @@
  */
 namespace enigmatix\select2;
 
+use yii\base\InvalidConfigException;
 use yii\base\Model;
 use yii\helpers\StringHelper;
 use yii\helpers\Url;
-
+use yii\helpers\Inflector;
 
 
 /**
@@ -24,11 +25,15 @@ use yii\helpers\Url;
 class Relate extends Select2
 {
     public $controller;
+
+    /**
+     * @var Model
+     */
+    public $fieldModel;
     public function run()
     {
         if($this->url == null)
             $this->url = $this->generateUrl();
-
         parent::run();
     }
 
@@ -38,16 +43,49 @@ class Relate extends Select2
     }
 
     protected function getController(){
-        $className = StringHelper::basename($model::className());
-        return strtolower($className);
+
+        if($this->controller != null){
+            return $this->controller;
+        }
+
+        $field      = $this->fieldModel != null ?: $this->getFieldModel();
+        if(method_exists($field, 'getController')){
+            return $field->getController();
+        }else{
+            $className = StringHelper::basename($field::className());
+
+            return Inflector::slug(Inflector::titleize($className));
+        }
     }
 
     protected function generateUrl(){
+            return  Url::to(['/'.$this->getController(), 'json' => '']);
+    }
 
-        $model      = $this->model;
-        $controller = method_exists($model, 'getController') ? $model->getController() : strtolower($this->getController());
+    protected function getFieldModel(){
+        $model = $this->model;
+        $field = str_replace('_id', '', $this->getFieldName());
 
-        return Url::to([$controller, 'json' => null]);
+        $activeQueryName = 'get' . Inflector::camelize($field);
+        if(!method_exists($model, $activeQueryName)){
+            $activeQueryName .= 's';
+            if(!method_exists($model, $activeQueryName))
+                throw new InvalidConfigException("No controller string, url string or fieldModel supplied in widget config, and default method $activeQueryName does not exist in " . $this->model->className());
+        }
+
+
+        /* @var \yii\db\ActiveQuery $activeQuery */
+
+        $activeQuery            = $model->$activeQueryName();
+        $fieldModelClassName    = $activeQuery->modelClass;
+
+        if($this->value != null){
+            return $fieldModelClassName::findOne($this->value);
+        } else {
+
+            return new $fieldModelClassName;
+        }
 
     }
+
 }
