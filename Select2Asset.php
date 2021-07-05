be6f89f8098017fd3581eb37f2dba3eceb8a0491
Joel Small <joel@premiumbreed.com>
2021-07-05 12:48:01 +0930

    Corrected asset path to npm

--- Select2Asset.php
+++ Select2Asset.php
@@ -12,7 +12,7 @@ use yii\web\AssetBundle;
 
 class Select2Asset extends AssetBundle
 {
-    public $sourcePath = '@vendor/bower/select2/dist';
+    public $sourcePath = '@npm/select2/dist';
     public $css = [
         'css/select2.min.css',
     ];
